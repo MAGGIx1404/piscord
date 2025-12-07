ff4a5f3e66a43860bea4f594c887884741cc3878
Jeet <jeetpatel0070070@gmail.com>
2025-12-07 13:26:16 +0530

    initial commit

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -1,15 +1,17 @@
 <template>
-  <main class="w-full h-screen grid grid-cols-2">
-    <figure
-      class="size-full"
-      :class="route.name === 'auth-register' ? 'order-2' : ''"
+  <main class="w-full h-screen overflow-hidden grid grid-cols-2">
+    <div
+      class="h-screen flex items-center justify-center py-8"
+      :class="route.name === 'auth-register' ? 'order-2 pr-8' : 'pl-8'"
     >
-      <img
-        src="/images/auth.png"
-        alt="Auth Image"
-        class="size-full scale-125"
-      />
-    </figure>
+      <figure class="size-full border-black border-4 rounded-xl">
+        <img
+          src="/images/auth.png"
+          alt="Auth Image"
+          class="size-full scale-125"
+        />
+      </figure>
+    </div>
     <div class="size-full flex items-center justify-center relative">
       <div
         class="absolute top-8 left-8 size-16 bg-accent p-2 rounded-lg border-2 border-black shadow-md"
--- app/pages/auth/register/index.vue
+++ app/pages/auth/register/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1>Register</h1>
+  <div class="w-1/2 flex flex-col gap-6">
+    <h1 class="text-5xl font-semibold">Create an Account</h1>
+  </div>
 </template>
 
 <script setup>
