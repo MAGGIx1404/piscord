b69e78a4b566189b744680dc5b699a6f295ffa56
Jeet <jeetpatel0070070@gmail.com>
2025-12-09 12:28:40 +0530

    add chat page

--- app/components/channel/chat-message.vue
+++ app/components/channel/chat-message.vue
@@ -1,53 +1,62 @@
 <template>
-  <div class="flex flex-col transition-all p-3 rounded-xl hover:bg-gray-100 relative">
-    <!-- Primary Chat of reply -->
+  <div class="relative">
     <div
-      v-if="replyToMessage"
-      class="ml-5 p-3 mb-2 flex items-center gap-2 rounded-xl transition-all hover:bg-gray-200"
+      class="max-w-1/2 flex items-end gap-2"
+      :class="chat.author.id === userId ? 'ml-auto flex-row-reverse' : ''"
     >
-      <Avatar class="border-2 border-black rounded-lg size-6">
-        <AvatarImage :src="replyToMessage.author.image" alt="@User" />
-        <AvatarFallback>
-          {{ replyToMessage.author.name.charAt(0) }}
-        </AvatarFallback>
+      <Avatar class="size-14 border-2 border-black rounded-lg">
+        <AvatarImage :src="chat.author.image" alt="@Server" />
+        <AvatarFallback>US</AvatarFallback>
       </Avatar>
-      <div>
-        <div class="flex items-center gap-1 text-gray-700 text-sm">
-          <span class="font-semibold">{{ replyToMessage.author.name }}</span>
-          <span>{{
-            replyToMessage.message.length > 100
-              ? replyToMessage.message.slice(0, 100) + "..."
-              : replyToMessage.message
-          }}</span>
-        </div>
-      </div>
-    </div>
 
-    <!-- Chat -->
-    <div class="flex items-start gap-3">
-      <Avatar class="border-2 border-black shadow-lg rounded-lg">
-        <AvatarImage :src="chat.author.image" alt="@User" />
-        <AvatarFallback>
-          {{ chat.author.name.charAt(0) }}
-        </AvatarFallback>
-      </Avatar>
-      <div>
-        <div class="flex items-center gap-2">
-          <span class="font-semibold">{{ chat.author.name }}</span>
-          <span class="text-sm text-gray-500"
-            >{{ new Date(chat.timestamp).toLocaleTimeString() }}
-          </span>
-        </div>
-        <p class="mt-1">{{ chat.message }}</p>
-      </div>
-    </div>
+      <!-- Message -->
+      <Card
+        class="w-full mb-2 gap-4"
+        :class="
+          chat.author.id === userId
+            ? 'rounded-br-none bg-primary text-primary-foreground'
+            : 'rounded-bl-none bg-accent'
+        "
+      >
+        <p class="text-base">
+          {{ chat.message }}
+        </p>
 
-    <!-- Chat's reactions -->
-    <div class="w-full"></div>
+        <!-- media -->
+        <div class="w-full grid grid-cols-2 gap-4" v-if="chat.images.length">
+          <figure
+            class="h-52 border-2 border-black rounded-xl"
+            v-for="media in chat.images"
+            :key="media"
+          >
+            <img :src="media" alt="chat media" class="size-full" />
+          </figure>
+        </div>
+
+        <div
+          class="w-full flex items-center justify-between"
+          :class="chat.author.id === userId ? 'flex-row-reverse' : ''"
+        >
+          <p class="text-xs">@{{ chat.author.id === userId ? "You" : chat.author.name }}</p>
+          <p class="text-xs">
+            {{
+              new Date(chat.timestamp).toLocaleTimeString([], {
+                hour: "2-digit",
+                minute: "2-digit",
+                hour12: true
+              })
+            }}
+          </p>
+        </div>
+      </Card>
+    </div>
   </div>
 </template>
 
 <script setup>
+const store = useUserStore();
+const userId = store.user.id;
+
 defineProps({
   chat: {
     type: Object,
